aa014a0cece697f7b965443d20309c06871e5e5a
Tony Cook <tony_cook@outlook.com>
2021-12-06 15:24:47 +1300

    debugging map function error

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function TaskList({tasksArray, deleteTaskFromArray}) {
-    tasksArray.map((task, index) => {
+    tasksArray.map((task, index) =>
         console.log(index, task)
-    })
+    )
 
     return (
         <div>
